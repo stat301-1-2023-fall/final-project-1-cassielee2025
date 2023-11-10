7d6da44909b49228dcb77e4aff9c2c02f092c09c
cassielee2025 <145501426+cassielee2025@users.noreply.github.com>
2023-11-09 23:01:38 -0600

    create a reprex to figure out why it's not working in tidy

--- functions_for_join.R
+++ functions_for_join.R
@@ -17,3 +17,30 @@ rename_value(age_demographics, value)
 # this doesn't work
 age_demographics %>% 
   rename_value(value)
+
+# make a reprex ----
+library(tidyverse)
+
+# random data
+mydata <- tibble(
+  variable1 = 1:5,
+  value = 6:10
+)
+
+# renaming "value" variable in all datasets ----
+rename_value <- function(df, variable) {
+  # copy name of dataset
+  name <- deparse(substitute(df))
+  
+  # change value to new name
+  df %>%
+    rename(!!name := {{variable}}) 
+}
+
+# why does this not work in tidy?
+# this works
+rename_value(mydata, value)
+
+# this doesn't work
+mydata %>% 
+  rename_value(value)
