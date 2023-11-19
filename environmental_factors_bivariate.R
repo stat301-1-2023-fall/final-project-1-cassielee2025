2e7546cb1d828320e8beeacaed2115201f0ab7dc
cassielee2025 <145501426+cassielee2025@users.noreply.github.com>
2023-11-19 16:09:51 -0600

    set up for environmental factors bivariate analysis

---
+++ environmental_factors_bivariate.R
@@ -0,0 +1,10 @@
+# load packages and data
+library(tidyverse)
+load("data/full_air_quality_data.rda")
+
+# pollutant variables:
+# days over O3 standard, days over 2.5 PM standard
+# highway living, highway schools
+# parks access
+# 5 pollutants
+# transportation type
