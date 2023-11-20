b373e7867b8c3f1e5adfefe316592cd29b9059a3
cassielee2025 <145501426+cassielee2025@users.noreply.github.com>
2023-11-20 13:09:30 -0600

    PM 2.5 bivariate

--- 06_environmental_factors_bivariate.R
+++ 06_environmental_factors_bivariate.R
@@ -184,3 +184,168 @@ full_data %>%
     y = "Days over ozone standard"
   )
 
+# PM 2.5 ----
+# percent living near highway
+full_data %>% 
+  bivariate_plot(highway_living, days_over_pm_standard) +
+  coord_cartesian(ylim = c(0, 20)) +
+  labs(
+    title = "Percent of days over PM 2.5 standard vs population living near a highway",
+    x = "Percent of people living within 150 M of a highway",
+    y = "Percent of days over PM 2.5 standard"
+  )
+
+# percent of schools near highway
+full_data %>% 
+  bivariate_plot(highway_schools, days_over_pm_standard) +
+  coord_cartesian(ylim = c(0, 20)) +
+  labs(
+    title = "Percent of days over PM 2.5 standard vs public schools near a highway",
+    x = "Percent of public schools within 150 M of a highway",
+    y = "Percent of days over PM 2.5 standard"
+  )
+
+# parks access
+full_data %>% 
+  bivariate_plot(parks_access, days_over_pm_standard) +
+  coord_cartesian(ylim = c(0, 20)) +
+  labs(
+    title = "Percent of days over PM 2.5 standard vs access to parks",
+    x = "Percent of people living within 1/2 mile of a park",
+    y = "Percent of days over PM 2.5 standard"
+  )
+
+# walking transportation
+full_data %>% 
+  bivariate_plot(transportation_type_walking, days_over_o3_standard) +
+  coord_cartesian(
+    xlim = c(0, 20),
+    ylim = c(0, 50)
+  ) +
+  labs(
+    title = "Percent of days over ozone standard vs main transportation: walking",
+    x = "Percent of people who mainly walk",
+    y = "Percent of days over ozone standard"
+  )
+
+# biking transportation
+full_data %>% 
+  bivariate_plot(transportation_type_bicycle, days_over_pm_standard) +
+  coord_cartesian(
+    xlim = c(0, 6),
+    ylim = c(0, 20)
+  ) +
+  labs(
+    title = "Percent of days over PM 2.5 standard vs main transportation: biking",
+    x = "Percent of people who mainly bike",
+    y = "Percent of days over PM 2.5 standard"
+  )
+# might have to do with urban areas being more bikable?
+
+# no transportation
+full_data %>% 
+  bivariate_plot(transportation_none, days_over_pm_standard) +
+  coord_cartesian(
+    xlim = c(0, 25),
+    ylim = c(0, 20)
+  ) +
+  labs(
+    title = "Percent of days over PM 2.5 standard vs main transportation: none",
+    x = "Percent of people who mainly do not use transportation",
+    y = "Percent of days over PM 2.5 standard"
+  )
+# might have to do with urban areas having more virtual workers?
+
+# driving alone
+full_data %>% 
+  bivariate_plot(occupancy_drove_alone, days_over_pm_standard) +
+  coord_cartesian(ylim = c(0, 50)) +
+  labs(
+    title = "Percent of days over PM 2.5 standard vs main transportation: driving alone",
+    x = "Percent of people who mainly drive alone",
+    y = "Percent of days over PM 2.5 standard"
+  )
+# what
+
+# carpooling
+full_data %>% 
+  bivariate_plot(occupancy_carpooled, days_over_pm_standard) +
+  coord_cartesian(
+    xlim = c(0, 20),
+    ylim = c(0, 20)
+  ) +
+  labs(
+    title = "Percent of days over PM 2.5 standard vs main transportation: carpooling",
+    x = "Percent of people who mainly carpool",
+    y = "Percent of days over PM 2.5 standard"
+  )
+
+# public transportation
+full_data %>% 
+  bivariate_plot(transportation_public, days_over_pm_standard) +
+  coord_cartesian(ylim = c(0, 20)) +
+  labs(
+    title = "Percent of days over PM 2.5 standard vs main transportation: public transportation",
+    x = "Percent of people who mainly use public transportation",
+    y = "Percent of days over PM 2.5 standard"
+  )
+
+# so these things are mostly showing an urban bias probably
+
+# what counties have the highest ozone?
+full_data %>% 
+  select(county, state, days_over_pm_standard) %>% 
+  st_drop_geometry() %>% 
+  slice_max(days_over_pm_standard, n = 10)
+
+# pollutant benzene
+full_data %>% 
+  bivariate_plot(pollutant_benzene, days_over_pm_standard) +
+  coord_cartesian(ylim = c(0, 20)) +
+  labs(
+    title = "Percent of days over PM 2.5 standard vs benzene pollution",
+    x = "Concentration of benzene (µg/m3)",
+    y = "Percent of days over PM 2.5 standard"
+  )
+
+# pollutant formaldehyde
+full_data %>% 
+  bivariate_plot(pollutant_formaldehyde, days_over_pm_standard) +
+  coord_cartesian(ylim = c(0, 20)) +
+  labs(
+    title = "Percent of days over PM 2.5 standard vs formaldehyde pollution",
+    x = "Concentration of formaldehyde (µg/m3)",
+    y = "Percent of days over PM 2.5 standard"
+  )
+
+# pollutant acetaldehyde
+full_data %>% 
+  bivariate_plot(pollutant_acetaldehyde, days_over_pm_standard) +
+  coord_cartesian(ylim = c(0, 20)) +
+  labs(
+    title = "Percent of days over PM 2.5 standard vs acetaldehyde pollution",
+    x = "Concentration of acetaldehyde (µg/m3)",
+    y = "Percent of days over PM 2.5 standard"
+  )
+
+# pollutant carbon tetrachloride
+full_data %>% 
+  bivariate_plot(pollutant_carbon_tetrachloride, days_over_pm_standard) +
+  coord_cartesian(ylim = c(0, 20)) +
+  labs(
+    title = "Percent of days over PM 2.5 standard vs carbon tetrachloride pollution",
+    x = "Concentration of carbon tetrachloride (µg/m3)",
+    y = "Percent of days over PM 2.5 standard"
+  )
+
+# pollutant 1,3-butadiene
+full_data %>% 
+  bivariate_plot(pollutant_1_3_butadiene, days_over_pm_standard) +
+  coord_cartesian(ylim = c(0, 20)) +
+  labs(
+    title = "Days over PM 2.5 standard vs 1,3-butadiene pollution",
+    x = "Concentration of 1,3-butadiene (µg/m3)",
+    y = "Days over PM 2.5 standard"
+  )
+
+
