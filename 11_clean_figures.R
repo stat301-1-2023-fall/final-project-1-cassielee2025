dd19677d09950e401c9d5a9701ee03ca0c51c776
cassielee2025 <145501426+cassielee2025@users.noreply.github.com>
2023-11-25 12:01:14 -0600

    working on days over

--- 11_clean_figures.R
+++ 11_clean_figures.R
@@ -34,7 +34,7 @@ p1 + p2
 full_data %>% 
   mutate(pm_standard = days_over_pm_standard/100 * 365) %>% 
   ggplot(aes(days_over_o3_standard, pm_standard)) +
-  geom_point() +
+  geom_jitter() +
   geom_smooth(method = lm, se = FALSE) +
   lims(
     x = c(0, 50),
